121071b6c8e2c433cb37b191adf6baa45a345004
EBRU SOMUNCU <57454859+EbruSomuncu@users.noreply.github.com>
2020-06-02 01:49:24 +0300

    Add files via upload

---
+++ Bölüm1/Ders1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Ders1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////Write=ekrana basar
+            ////WriteLine=alt satıra geçerek ekrana basar
+            //Console.WriteLine("Hello World!");
+
+            //Console.Write("Ebru Somuncu");
+
+            //Console.WriteLine("15+3");
+            //Console.WriteLine(15 + 3);
+
+            //Console.WriteLine("Adınızı giriniz:");
+            //string metin = Console.ReadLine();
+            //Console.WriteLine("Hoşgeldin " + metin
+
+            ////İki Sayının Toplamı
+            //Console.WriteLine("Sayı1:");
+            //int sayi1=Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Sayı2:");
+            //int sayi2 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Toplam:" + (sayi1 + sayi2));
+
+            ////Dairenin Alanı
+            //Console.WriteLine("Yarıçap(r):");
+            //int r=Convert.ToInt32(Console.ReadLine());
+            //double pi = 3.14;
+            //double alan = pi * r * r;
+            //Console.WriteLine("Yarıçapı " + r + " olan Dairenin alanı: " + alan);
+
+            //Console.WriteLine("Karakter giriniz:");
+            //int karakter=Convert.ToChar(Console.ReadLine());
+            ////int karakter=char.Parse(Console.ReadLine());
+            //Console.WriteLine("Girilen Karakter:" + karakter);
+
+            //// Klavyeden farklı türde veri çekmek:
+            ////Convert.ToInt32();
+            ////Convert.ToDouble();
+            ////Convert.ToBoolen();
+            ////Convert.toChar();
+
+            Boolean dogrumu =true;
+            Console.WriteLine(dogrumu);
+            dogrumu = false;
+            Console.WriteLine(dogrumu);
+
+        }
+    }
+}
