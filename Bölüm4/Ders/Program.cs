908fa394a622228a37f9b1f538e3d65bde5d2265
EBRU SOMUNCU <57454859+EbruSomuncu@users.noreply.github.com>
2020-06-05 19:43:47 +0300

    Add files via upload

---
+++ Bölüm4/Ders/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace Ders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("DÖNGÜLER");
+            //Console.WriteLine("------------");
+
+            //while(true)
+            //{
+            //Console.WriteLine("Merhaba Arkadaşlar");
+            //}
+
+            //Console.WriteLine("------------");
+            //Console.WriteLine("Kelime giriniz..");
+            //string str = Console.ReadLine();
+
+            //while (str != "iptal")
+            //{
+            //    Console.WriteLine("Kelime giriniz..");
+            //    string kelime = Console.ReadLine();
+            //    str = kelime;
+
+            //}
+            //Console.WriteLine("İptal ediliyor..");
+
+            //Console.WriteLine("------------");
+
+            //for (int i = 5; i < 26; i += 5)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //Console.WriteLine("------------");
+
+            //Console.WriteLine("------------");
+            //Console.WriteLine("Sayı giriniz..");
+            //int sayi = Convert.ToInt32(Console.ReadLine());
+
+            //for(int i=sayi;i>0;i--)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+            //Console.WriteLine("------------");
+            //Console.WriteLine("Factoriyeli bulunacak sayıyı giriniz..");
+            //int sayi = Convert.ToInt32(Console.ReadLine());
+            //int fact = 1;
+            //for (int i = sayi; i > 1; i--)
+            //{
+            //    fact *= i;
+
+            //}
+
+            //Console.WriteLine(sayi+",sayısının factoriyeli="+fact);
+
+            //Console.WriteLine("------------");
+
+            //for(int i=1;i<=10;i++)
+            //{
+            //    for(int j=1;j<=10;j++)
+            //    {
+            //        Console.WriteLine(i + "x" + j + "=" + (i * j));
+            //    }
+            //    Console.WriteLine("------------");
+            //}
+            //Console.WriteLine("------------");
+
+            //Console.WriteLine("Sayı giriniz..");
+            //int sayi = Convert.ToInt32(Console.ReadLine());
+            //for (int j = 1; j <= 10; j++)
+            //{
+            //    Console.WriteLine(sayi + "x" + j + "=" + (sayi * j));
+            //}
+
+            Console.WriteLine("------------");
+
+
+
+
+        }
+    }
+}
